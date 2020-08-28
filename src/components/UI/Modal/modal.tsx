64ad51ba8b5322af7bd35b9e6645b1648b0696e3
Anurag <aarwalka@cisco.com>
2020-08-28 23:09:00 +0530

    improved performance

--- src/components/UI/Modal/modal.tsx
+++ src/components/UI/Modal/modal.tsx
@@ -2,20 +2,34 @@ import React, { Fragment } from 'react';
 import styles from './modal.module.css';
 import Backdrop from '../Backdrop/Backdrop';
 
-const Modal = ({ children, show, hide }: any) => {
+export interface ModalProps {
+    show: any,
+    hide: any
+}
 
-    return (
-        <Fragment>
-        <Backdrop show={show} hide={hide}/>
-        <div className={styles.Modal} style={{
-            transform: show ? 'translateY(0)' : 'translateY(-100vh)',
-            opacity: show ? '1' : '0'
-        }}>
-            {children}
-        </div>
-        </Fragment>
-    )
+export interface ModalState {
 
+}
+
+class Modal extends React.Component<ModalProps, ModalState> {
+    shouldComponentUpdate(nextProps: any, nextState: any) {
+        return this.props.show !== nextProps.show
+    }
+
+    render() {
+        const { children, show, hide } = this.props;
+        return (
+            <Fragment>
+                <Backdrop show={show} hide={hide} />
+                <div className={styles.Modal} style={{
+                    transform: show ? 'translateY(0)' : 'translateY(-100vh)',
+                    opacity: show ? '1' : '0'
+                }}>
+                    {children}
+                </div>
+            </Fragment>
+        );
+    }
 }
 
 export default Modal;
--- src/components/burger/orderSummary/orderSummary.tsx
+++ src/components/burger/orderSummary/orderSummary.tsx
@@ -1,11 +1,16 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import Button from '../../UI/Button/Button';
 
 const OrderSummary = ({ingredients, hide, prurchase, price}: any) => {
+    useEffect(() => {
+        console.log('s')
+    })
     const ingredientSummary = Object.keys(ingredients)
     .map(key => {
-    return <li key={key}><span style={{ textTransform: 'capitalize'}}>{key}</span> : {ingredients[key]}</li>
+    return <li key={key}><span style={{ textTransform: 'capitalize'}}>
+        {key}</span> : {ingredients[key]}</li>
     })
+
     return <Fragment>
         <h3>Your Order</h3>
         <p>A delicious burger with following ingredients:</p>
