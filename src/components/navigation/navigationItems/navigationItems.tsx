958adfad8a583bd0bb947f808f3ebba137fb0a70
Anurag <aarwalka@cisco.com>
2020-09-04 23:35:17 +0530

    fixed active link issue

--- src/components/navigation/navigationItems/navigationItems.tsx
+++ src/components/navigation/navigationItems/navigationItems.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import classes from './navigationItems.module.css';
 import NavigationItem from './navigationItem/navigationItem';
+import { withRouter } from 'react-router-dom';
 
-const NavigationItems = () => (
-    <ul className={classes.NavigationItems}>
-        <NavigationItem link="/" active>Burger Builder</NavigationItem>
-        <NavigationItem link="/checkout">Check Out</NavigationItem>
-    </ul>
-);
+const NavigationItems = ({ location: { pathname } }: any) => {
+    const myOrders = '/my-orders';
+    const burgerBuilderLink = '/';
+    return (<ul className={classes.NavigationItems}>
+        <NavigationItem link={burgerBuilderLink} >Burger Builder</NavigationItem>
+        <NavigationItem link={myOrders} >My Orders</NavigationItem>
+    </ul>)
+}
 
-export default NavigationItems;
+export default withRouter(NavigationItems);
