47100997bd965206371c76428ea59ef0b78a988f
Anurag <aarwalka@cisco.com>
2020-09-06 16:56:52 +0530

    code format

--- src/components/burger/buildControl/buildControls.tsx
+++ src/components/burger/buildControl/buildControls.tsx
@@ -3,23 +3,23 @@ import styles from './buildControls.module.css';
 import BuildControl from './buildControl/buildControl';
 
 const controls = [
-    {label: 'Salad', type: 'salad'},
-    {label: 'Bacon', type: 'bacon'},
-    {label: 'Cheese', type: 'cheese'},
-    {label: 'Meat', type: 'meat'}
+    { label: 'Salad', type: 'salad' },
+    { label: 'Bacon', type: 'bacon' },
+    { label: 'Cheese', type: 'cheese' },
+    { label: 'Meat', type: 'meat' }
 ]
 
-const BuildControls = ({moreClicked, lessClicked, disabled, totalPrice, purchasable, orderNow}: any) => {
+const BuildControls = ({ moreClicked, lessClicked, disabled, totalPrice, purchasable, orderNow }: any) => {
     return (
         <div className={styles.buildControls}>
-            <p>Current Price <strong>${totalPrice.toFixed(2)}</strong></p>
-            {controls.map(({label, type}) => <BuildControl 
-            label={label} 
-            moreClicked={moreClicked} 
-            lessClicked={lessClicked}
-            type={type} 
-            disabled={disabled[type]}
-            key={label}></BuildControl>)}
+            <p>Current Price <strong>${totalPrice}</strong></p>
+            {controls.map(({ label, type }) => <BuildControl
+                label={label}
+                moreClicked={moreClicked}
+                lessClicked={lessClicked}
+                type={type}
+                disabled={disabled[type]}
+                key={label}></BuildControl>)}
             <button className={styles.OrderButton} disabled={purchasable} onClick={orderNow}>Order Now</button>
         </div>
     )
