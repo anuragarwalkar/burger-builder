0d4d8ead472d31a1f0c573d2f21589efcc0572d8
Anurag <aarwalka@cisco.com>
2020-09-06 16:57:20 +0530

    code format

--- src/components/burger/orderSummary/orderSummary.tsx
+++ src/components/burger/orderSummary/orderSummary.tsx
@@ -1,21 +1,21 @@
 import React, { Fragment } from 'react';
 import Button from '../../UI/Button/Button';
 
-const OrderSummary = ({ingredients, hide, prurchase, price}: any) => {
-    
+const OrderSummary = ({ ingredients, hide, prurchase, price }: any) => {
+
     const ingredientSummary = Object.keys(ingredients)
-    .map(key => {
-    return <li key={key}><span style={{ textTransform: 'capitalize'}}>
-        {key}</span> : {ingredients[key]}</li>
-    })
+        .map(key => {
+            return <li key={key}><span style={{ textTransform: 'capitalize' }}>
+                {key}</span> : {ingredients[key]}</li>
+        })
 
     return <Fragment>
         <h3>Your Order</h3>
         <p>A delicious burger with following ingredients:</p>
         <ul>
-             {ingredientSummary}
+            {ingredientSummary}
         </ul>
-        <p>Total Price: <span>{price.toFixed(2)}</span></p>
+        <p>Total Price: <span>{price}</span></p>
         <p>Continue to check out.</p>
         <Button click={hide} btnType="Danger">Cancel</Button>
         <Button click={prurchase} btnType="Success">Continue</Button>
