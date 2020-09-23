a0954d7425d0f98e1b1bfbd6caaa48e3c2fb7d05
Anurag <aarwalka@cisco.com>
2020-09-23 19:10:41 +0530

    fixed modal condition

--- src/components/UI/Modal/modal.tsx
+++ src/components/UI/Modal/modal.tsx
@@ -11,7 +11,7 @@ export interface ModalProps {
 
 const shouldComponentUpdate = (prevProps: ModalProps, nextProps: ModalProps) => {
     return (
-        prevProps.show === nextProps.show ||
+        prevProps.show === nextProps.show &&
         nextProps.children === prevProps.children
     );
   };
